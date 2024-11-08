540b951ee228fb14ef586239328e99c320f7a776
Irawo <timothyaremu63@gmail.com>
2024-11-08 01:26:16 +0100

    added a redirect to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+"use client"
+import { useRouter } from "next/router";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to /landing-page as soon as the component is mounted
+    router.push("/landing-page");
+  }, [router]);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
