10796cf76b826416bc386b2fed50a8ea9317727c
Irawo <timothyaremu63@gmail.com>
2024-11-08 01:40:42 +0100

    revert the next config file and updated the home page to landing page

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Your existing config options here
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/landing-page',
-        permanent: true, // You can set this to true if it's a permanent redirect (301) or false for temporary (302)
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-"use client"
-import { useRouter } from "next/router";
-import styles from "./page.module.css";
-import { useEffect } from "react";
+import LandingPageLayout from "@/features/landingPage/landingPageLayout/LandingPageLayout";
+import React from 'react'
+
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to /landing-page as soon as the component is mounted
-    router.push("/landing-page");
-  }, [router]);
-
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        home
-      </main>
-    </div>
+    <main>
+      <LandingPageLayout />
+    </main>
   );
 }
