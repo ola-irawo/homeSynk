1894563de1caad37ccce9635a2c9557c179f6ec2
Irawo <timothyaremu63@gmail.com>
2024-11-15 22:22:58 +0100

    updated contactUs action

--- src/app/server-actions/landing-page/actions.ts
+++ src/app/server-actions/landing-page/actions.ts
@@ -16,18 +16,18 @@ export const joinWaitlist = async (formData: FormData) => {
 export const contactUs = async (formData: FormData) => {
     const name = formData.get("name") as string;
     const email = formData.get("email") as string;
-    const phone = formData.get("phone") as string;
+    const number = formData.get("number") as string;
     const subject = formData.get("subject") as string;
     const message = formData.get("message") as string;
 
-    console.log(name, email, phone, subject, message );
+    console.log({name, email, number, subject, message} );
 
     await fetch(`${apiUrl}/api/form/submit`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ name, email, phone, subject, message }),
+        body: JSON.stringify({ name, email, number, subject, message }),
     });
 
 };
