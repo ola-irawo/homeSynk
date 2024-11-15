5b019da8b9dec93247013aee67f9d1340ca0da53
Irawo <timothyaremu63@gmail.com>
2024-11-15 22:07:21 +0100

    fix the server error

--- src/app/server-actions/landing-page/actions.ts
+++ src/app/server-actions/landing-page/actions.ts
@@ -1,36 +1,35 @@
-const apiUrl = "https://homesync.onrender.com"
-
+const apiUrl = "https://homesync.onrender.com";
 
 export const joinWaitlist = async (formData: FormData) => {
-    const email = formData.get("email")
+    const email = formData.get("email") as string;
 
     const response = await fetch(`${apiUrl}/api/createRequest`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email}),
+        body: JSON.stringify({ email }),
     });
 
-    const data = await response.json()
-}
+    const data = await response.json();
+};
 
-export const contactUs = async (formdData: FormData) => {
-    const name = formdData.get("name")
-    const email = formdData.get("email")
-    const phone = formdData.get("phone")
-    const subject = formdData.get("subject")
-    const message = formdData.get("message")
+export const contactUs = async (formData: FormData) => {
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const phone = formData.get("phone") as string;
+    const subject = formData.get("subject") as string;
+    const message = formData.get("message") as string;
 
-    console.log(formdData.get("name"))
+    console.log(name);
 
     const response = await fetch(`${apiUrl}/api/form/submit`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({ name, email, phone, subject, message }),
     });
 
-    const data = await response.json()
-}
+    const data = await response.json();
+};
