008c5dcec4e0b0a97e571a4f8b416dbc1978348f
Irawo <timothyaremu63@gmail.com>
2025-03-15 10:53:05 +0100

    u

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    ignoreDuringBuilds: true, // This will skip ESLint checks during builds
+    ignoreDuringBuilds: true, // This will skip ESLint checks during build
   },
 };
 
